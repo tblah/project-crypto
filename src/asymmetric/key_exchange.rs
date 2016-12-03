d4bf022764dade3747e0f94a35e44267b1c894f3
Tom Eccles <t@freedommail.info>
2016-12-03 19:38:15 +0000

    (Breaking interface change) key_exhange stuff is in it's own submodule of asymmetric

--- src/asymmetric/key_exchange.rs
+++ src/asymmetric/key_exchange.rs
@@ -32,7 +32,7 @@
 //! ```
 //! # extern crate sodiumoxide;
 //! # extern crate proj_crypto;
-//! # use proj_crypto::asymmetric::*;
+//! # use proj_crypto::asymmetric::key_exchange::*;
 //! use std::str;
 //! 
 //! # fn main() {
@@ -108,7 +108,35 @@ use sodiumoxide::crypto::hash::sha256;
 use sodiumoxide::utils::memzero;
 use sodiumoxide::randombytes;
 use sodiumoxide::utils::memcmp;
-use super::*;
+
+/// Public Key - just an alias. Implements drop() so the memory will be wiped when it goes out of scope
+pub type PublicKey = curve25519::GroupElement; 
+/// The number of bytes in a PublicKey
+pub const PUBLIC_KEY_BYTES: usize = curve25519::GROUPELEMENTBYTES;
+/// create a public key from a slice
+pub fn public_key_from_slice(slice: &[u8]) -> Option<PublicKey> { curve25519::GroupElement::from_slice(slice) }
+
+/// Secret Key - just an alias. Implements drop() so the memory will be wiped when it goes out of scope
+pub type SecretKey = curve25519::Scalar;
+/// The number of bytes in a SecretKey
+pub const SECRET_KEY_BYTES: usize = curve25519::SCALARBYTES;
+/// create a secret key from a slice
+pub fn secret_key_from_slice(slice: &[u8]) -> Option<SecretKey> { curve25519::Scalar::from_slice(slice) }
+
+/// Generate an asymmetric key pair.
+pub fn gen_keypair() -> (PublicKey, SecretKey) {
+    let mut sk_bytes_raw = randombytes::randombytes(sha256::BLOCKBYTES);
+    let mut sk_hash = sha256::hash(&sk_bytes_raw);
+    let &mut sha256::Digest(ref mut sk_bytes) = &mut sk_hash;
+
+    let sk = curve25519::Scalar::from_slice(&sk_bytes[0..curve25519::SCALARBYTES]).unwrap();
+    let pk = curve25519::scalarmult_base(&sk);
+
+    memzero(sk_bytes); // also kills off sk_hash
+    memzero(sk_bytes_raw.as_mut_slice());
+
+    (pk, sk) // both implement drop() to clear the memory so don't worry about them being copied
+}
 
 /// Stores long term keys (e.g. from a certificate authority)
 /// The secret key is safely erased from memory when this goes out of scope
@@ -282,7 +310,7 @@ pub fn server_verify_response(session_keys: &SessionKeys, response: &Vec<u8>, me
 /******************* Tests *******************/
 #[cfg(test)]
 mod tests {
-    use super::super::*;
+    use super::*;
     extern crate sodiumoxide;
     use sodiumoxide::randombytes;
 
--- src/asymmetric/mod.rs
+++ src/asymmetric/mod.rs
@@ -1,6 +1,8 @@
 //! Asymmetric encryption module.
 //! 
 //! self::key_exchange is re-exported for backwards compatibility. See that module for it's tests and documentation.
+//!
+//! Unfortunately key_exchange and sign have to use a different representation of the keypair because libsodium uses incompatible representations of the public key between signatures and key exchanges (public interface to curve25519_mult_base).
 
 /*  This file is part of project-crypto.
     project-crypto is free software: you can redistribute it and/or modify
@@ -15,47 +17,9 @@
     along with project-crypto.  If not, see http://www.gnu.org/licenses/.*/
 
 pub mod key_exchange;
-pub mod sign;
+//pub mod sign;
 
-pub use self::key_exchange::*;
 
-use sodiumoxide::crypto::scalarmult::curve25519;
-use sodiumoxide::randombytes;
-use sodiumoxide::utils::memzero;
-use sodiumoxide::crypto::hash::sha512;
 
-/// Public Key - just an alias. Implements drop() so the memory will be wiped when it goes out of scope
-pub type PublicKey = curve25519::GroupElement; 
-/// The number of bytes in a PublicKey
-pub const PUBLIC_KEY_BYTES: usize = curve25519::GROUPELEMENTBYTES;
-/// create a public key from a slice
-pub fn public_key_from_slice(slice: &[u8]) -> Option<PublicKey> { curve25519::GroupElement::from_slice(slice) }
-
-/// Secret Key - just an alias. Implements drop() so the memory will be wiped when it goes out of scope
-pub type SecretKey = curve25519::Scalar;
-/// The number of bytes in a SecretKey
-pub const SECRET_KEY_BYTES: usize = curve25519::SCALARBYTES;
-/// create a secret key from a slice
-pub fn secret_key_from_slice(slice: &[u8]) -> Option<SecretKey> { curve25519::Scalar::from_slice(slice) }
-
-/// Generate an asymmetric key pair.
-pub fn gen_keypair() -> (PublicKey, SecretKey) {
-    let mut sk_bytes_raw = randombytes::randombytes(sha512::BLOCKBYTES);
-    let mut sk_hash = sha512::hash(&sk_bytes_raw);
-    let &mut sha512::Digest(ref mut sk_bytes) = &mut sk_hash;
-
-    // I don't know why but libsodium does this
-    /*sk_bytes[0] &= 248;
-    sk_bytes[31] &= 63;
-    sk_bytes[31] |= 64;
-    */ 
-    let sk = curve25519::Scalar::from_slice(&sk_bytes[0..curve25519::SCALARBYTES]).unwrap();
-    let pk = curve25519::scalarmult_base(&sk);
-
-    memzero(sk_bytes); // also kills off sk_hash
-    memzero(sk_bytes_raw.as_mut_slice());
-
-    (pk, sk) // both implement drop() to clear the memory so don't worry about them being copied
-}
 
 
